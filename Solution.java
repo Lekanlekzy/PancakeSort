0c3079e5e1773df55899a0dd6ad93b997208461d
Lekanlekzy <81918799+Lekanlekzy@users.noreply.github.com>
2023-02-15 10:30:32 -0700

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static void pancakeSort(int[] inputArray) {
+        
+    }
+    
+    private static int find(int[] A, int target) {
+      
+    }
+    private static void flip(int[] A, int index) {
+        
+    }
+
+    public static void main(String[] args){
+        int[] arr = {3,2,4,1};
+        pancakeSort(arr);
+    }
+}
